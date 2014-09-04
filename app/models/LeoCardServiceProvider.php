e5b3f74df6714d3f043d8b7d481532514b19e9b7
Thibaud <thibaud@dauce.fr>
2014-09-04 10:40:08 +0200

    Add comment in ServiceProvider

--- app/models/LeoCardServiceProvider.php
+++ app/models/LeoCardServiceProvider.php
@@ -4,6 +4,11 @@ use Illuminate\Support\ServiceProvider;
 
 class LeoCardServiceProvider extends ServiceProvider  {
 
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
   public function register()
   {
     $this->app->bind('UserRepository', 'DatabaseUserRepository');

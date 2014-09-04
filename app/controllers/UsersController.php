c07b7751ff42cfd788538aac79ddabd9d2760b61
Thibaud <thibaud@dauce.fr>
2014-09-04 10:11:24 +0200

    Add HTTP code

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -22,26 +22,38 @@ class UsersController extends Controller {
     // On récupère les données envoyées via POST.
     $batch = Input::get('data');
 
-    // On suppose que le retour est correct.
-    $result = [
-      'valid' => true
-    ];
+    // Si les données envoyées sont vides, on retourne une erreur 400.
+    if (empty($batch))
+    {
+      return Response::json([
+        'valid' => false,
+        'error' => [
+          'code' => 1,
+          'message' => 'Les données envoyées sont vides.'
+        ]
+      ], 400);
+    }
 
     // On récupère toutes les entrées correspondant aux numéros de carte NFC.
-    $result['data'] = $this->userRepository->getBySerialBatch($batch);
+    $data = $this->userRepository->getBySerialBatch($batch);
 
-    // Si le retour n'est pas complet, on définit une erreur et un résultat invalide.
+    // Si le retour n'est pas complet, on retourne le résultat avec un code HTTP 206 Partial Content.
     if (count($result['data']) != count($batch))
     {
-      $result['errors'] = [
-        'code' => 1,
-        'message' => 'Toutes les données n\'ont pas pu être récupérées.'
-      ];
-
-      $result['valid'] = false;
+      return Response::json([
+        'data'  => $data,
+        'valid' => false,
+        'error' => [
+          'code' => 2,
+          'message' => 'Toutes les données n\'ont pas pu être récupérées.'
+        ]
+      ], 206);
     }
 
     // On retourne le résultat formaté en json.
-    return Response::json($result);
+    return Response::json([
+      'data'  => $data
+      'valid' => true,
+    ]);
   }
 }
